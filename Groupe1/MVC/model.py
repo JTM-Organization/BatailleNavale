a5d0596985087fb895efa375eb4529279b1546c7
SteelPotathor <97548610+SteelPotathor@users.noreply.github.com>
2022-02-26 23:21:59 +0100

    Update model.py

--- Groupe1/MVC/model.py
+++ Groupe1/MVC/model.py
@@ -11,6 +11,8 @@ global taille_de_carres
 taille_de_carres = hauteur//nombre_de_carres
 global grille
 grille = [[0 for colonne in range(nombre_de_carres)] for ligne in range(nombre_de_carres)]
+global vainqueur 
+vainqueur = 1
 
 global cnv
 root = Tk()
@@ -21,9 +23,20 @@ def reInitialisation():
     for i in range(nombre_de_carres):
         for j in range(nombre_de_carres):
             grille[i][j] = 0
+    global vainqueur 
+    vainqueur = 1
             
 def abandon_de_partie():
-    return 0
+    root.quit()
 
 def victoire():
-    return 0
+    global vainqueur
+    if grille[0][0]==1:
+        vainqueur = 0
+    return vainqueur
+
+def fin_de_partie():
+    for i in range(nombre_de_carres):
+        for j in range(nombre_de_carres):
+            grille[i][j] = 1
+            
