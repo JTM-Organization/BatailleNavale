1d5d13df738b210011c0799296d0932da3685ed0
L0rest <97474601+L0rest@users.noreply.github.com>
2022-03-01 19:04:11 +0100

    Add files via upload

--- Groupe1/MVC/1.1.3.1/view.py
+++ Groupe1/MVC/1.1.3.1/view.py
@@ -1,4 +1,5 @@
 #placer ici la gestion de l'interface graphique
+from tkinter import ANCHOR, CENTER
 from model import *
 
 #Réalisation des tirs (alliés et ennemis) :
@@ -34,7 +35,7 @@ def clic_plateau2(event):
             plateau2.itemconfigure(i * 10 + j + 1, fill="#80ff00")
             print(grille2)
             print("Ligne=", i + 1, "Colonne=", j + 1)
-            label1['text'] = "votre tour"
+            label1['text'] = "Votre tour"
             label1['bg'] = "green"
         else:
             print("Veuillez sélectionner une autre case")
@@ -67,7 +68,6 @@ def spiderman_do_a_flip(event):
     if (x + height <= 620) and (y + width <= 710) :
         widget.configure(width=height, height=width)
 
-
 def identify(event):
     widget = event.widget
     x = widget.winfo_x()
@@ -80,7 +80,6 @@ def identify(event):
         widget.place(x=new_x, y=new_y)
     else: 
         widget.place(x=630, y=122)
-    print(x,y)
 
 def bateaux():
     canvas1.bind("<Button-1>",drag_start)
@@ -127,18 +126,21 @@ def remplir_plateau2():
 def placer_boutons():
     global bouton1
     bouton1 = Button(root, text="Rejouer", command=recommencer, state=DISABLED)
-    bouton1.place(x = 750, y = 2)
+    bouton1.place(relx = 0.5, rely = 0.02, anchor='center')
     global bouton2
     bouton2 = Button(root, text="Quitter", command=abandon_de_partie)
-    bouton2.place(x = 751, y = 32)
+    bouton2.place(relx = 0.5, rely = 0.055, anchor='center')
+    global bouton3
+    bouton3 = Button(root, text="Confirmer", command=confirmer_placement)
+    bouton3.place(relx = 0.5, rely = 0.75, anchor='center')
     
 def placer_label():
     global label1
     label1 = Label(root, text="Votre tour", bg = "green")
-    label1.place(x = 746, y = 62)
+    label1.place(relx = 0.5, rely = 0.09, anchor='center')
     global label_victoire
     label_victoire = Label(root, text="")
-    label_victoire.place(x = 750, y = 92)
+    label_victoire.place(relx = 0.5, rely = 0.12, anchor='center')
     global label_scoreboard
     label_scoreboard = Label(root, text="SCORE:" + str(score))
     label_scoreboard.place(x = 0, y = 0)
@@ -170,7 +172,89 @@ def recommencer():
     label_victoire['text']=""
     global tour
     tour = 1
-            
+
+#Confirmation de la mise en place des bateaux:
+def confirmer_placement():
+    x1 = (canvas1.winfo_y() - dep_y)//taille_de_carres
+    y1 = (canvas1.winfo_x() - dep_x)//taille_de_carres
+    width1 = canvas1.winfo_width()
+    height1 = canvas1.winfo_height()
+    x2 = (canvas2.winfo_y()-100)//taille_de_carres
+    y2 = (canvas2.winfo_x()-10)//taille_de_carres
+    width2 = canvas2.winfo_width()
+    height2 = canvas2.winfo_height()
+    x3 = (canvas3.winfo_y()-100)//taille_de_carres
+    y3 = (canvas3.winfo_x()-10)//taille_de_carres
+    width3 = canvas3.winfo_width()
+    height3 = canvas3.winfo_height()
+    x4 = (canvas4.winfo_y()-100)//taille_de_carres
+    y4 = (canvas4.winfo_x()-10)//taille_de_carres
+    width4 = canvas4.winfo_width()
+    height4 = canvas4.winfo_height()
+    x5 = (canvas5.winfo_y()-100)//taille_de_carres
+    y5 = (canvas5.winfo_x()-10)//taille_de_carres
+    width5 = canvas5.winfo_width()
+    height5 = canvas5.winfo_height()
+
+    if (width1 > height1):
+        for i in range(width1//height1):
+            grille1[x1][y1+i] = 3
+            plateau1.itemconfigure(x1 * 10 + y1+i + 1, fill="gray")
+    elif (width1 < height1):
+        for i in range(height1//width1):
+            grille1[x1+i][y1] = 3
+            plateau1.itemconfigure((x1+i) * 10 + y1 + 1, fill="gray")
+
+    if (width2 > height2):
+        for i in range(width2//height2):
+            grille1[x2][y2+i] = 3
+            plateau1.itemconfigure(x2 * 10 + y2+i + 1, fill="gray")
+    elif (width2 < height2):
+        for i in range(height2//width2):
+            grille1[x2+i][y2] = 3
+            plateau1.itemconfigure((x2+i) * 10 + y2 + 1, fill="gray")
+    
+    if (width3 > height3):
+        for i in range(width3//height3):
+            grille1[x3][y3+i] = 3
+            plateau1.itemconfigure(x3 * 10 + y3+i + 1, fill="gray")
+    elif (width3 < height3):
+        for i in range(height3//width3):
+            grille1[x3+i][y3] = 3
+            plateau1.itemconfigure((x3+i) * 10 + y3 + 1, fill="gray")
+    
+    if (width4 > height4):
+        for i in range(width4//height4):
+            grille1[x4][y4+i] = 3
+            plateau1.itemconfigure(x4 * 10 + y4+i + 1, fill="gray")
+    elif (width4 < height4):
+        for i in range(height4//width4):
+            grille1[x4+i][y4] = 3
+            plateau1.itemconfigure((x4+i) * 10 + y4 + 1, fill="gray")
+
+    if (width5 > height5):
+        for i in range(width5//height5):
+            grille1[x5][y5+i] = 3
+            plateau1.itemconfigure(x5 * 10 + y5+i + 1, fill="gray")
+    elif (width5 < height5):
+        for i in range(height5//width5):
+            grille1[x5+i][y5] = 3
+            plateau1.itemconfigure((x5+i) * 10 + y5 + 1, fill="gray")
+
+    compteur = 0
+    for i in range(nombre_de_carres):
+        for j in range(nombre_de_carres):
+            if grille1[i][j] == 3:
+                compteur += 1
+    
+    if (compteur != 17):
+        recommencer()
+        canvas1.place(x=630, y=320)
+        canvas2.place(x=630, y=122)
+        canvas3.place(x=700, y=122)
+        canvas4.place(x=770, y=122)
+        canvas5.place(x=840, y=122)
+
 def affichage():
     global main
     remplir_plateau1()
